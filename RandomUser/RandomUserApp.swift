b39dd443e0eeec7558278431b4e99a59bba4ec23
laetuz <laetuzg@gmail.com>
2024-02-04 22:45:36 +0700

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Ryo Martin on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
