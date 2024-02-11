61ad5e266585f16a59b4c99f428d11f426bd46bd
laetuz <laetuzg@gmail.com>
2024-02-12 03:36:16 +0700

    wrapped into array to list the user into separate texts array objects.

--- RandomUser/UsersView.swift
+++ RandomUser/UsersView.swift
@@ -12,13 +12,15 @@ struct UsersView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Raw JSON Data: ")
-                ScrollView {
-                    Text(userData.users)
-                }
+            List(userData.users) { user in
+                Text(user.fullName)
             }
-            .padding()
+//            VStack {
+//                Text("Raw JSON Data: ")
+//                ScrollView {
+//                 //   Text(userData.users)
+//                }
+//            }
             .navigationTitle("Random Users")
         }
     }
