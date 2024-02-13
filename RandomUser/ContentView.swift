9bb0e96e819e2579039b9fa0663f5acb160f875f
laetuz <laetuzg@gmail.com>
2024-02-13 21:25:43 +0700

    Implement image fetching from the Internet.

--- RandomUser/ContentView.swift
+++ RandomUser/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AsyncImage(url: URL(string: "https://i0.wp.com/www.sciencenews.org/wp-content/uploads/2023/06/062623_CG_Megalodon_feat.jpg"), scale: 2) { image in
+                image.resizable().scaledToFill()
+            } placeholder: {
+                ProgressView()
+            }
+                .frame(
+                width: 200,
+                height: 100
+                )
+                .background(Color.blue)
+            Spacer(minLength: 20)
             UsersView()
         }
         .padding()
